5906643bccf2e051df9500855d32f80a57ed56bd
Bihan Banerjee <bihanbanerjee@Bihans-MacBook-Pro.local>
2024-06-11 12:20:22 +0530

    Updated app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
-import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { SignUp } from './pages/SignUp'
 import { SignIn } from './pages/SignIn'
 import { Blog } from './pages/Blog'
+import { Blogs } from "./pages/Blogs";
+import { Publish } from './pages/Publish';
 
 function App() {
 
@@ -13,8 +14,10 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/publish" element={<Publish />} />
         </Routes>
-      </BrowserRouter>    
+      </BrowserRouter>
     </>
   )
 }
